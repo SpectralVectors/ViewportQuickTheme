72de1ac6187e8f958a681cbef89f56cee7e93a89
SpectralVectors <robmuir1@gmail.com>
2021-06-30 11:05:05 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "Viewport QuickTheme",
+    "author": "Spectral Vectors",
+    "version": (0, 3),
+    "blender": (2, 90, 0),
+    "location": "View 3D",
+    "description": "Shortcuts and presets for the Viewport theme",
+    "warning": "",
+    "doc_url": "",
+    "category": "View 3D",
+}
+
+from .ViewportQuickTheme import *
